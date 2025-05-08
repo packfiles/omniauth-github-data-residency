c271f7c4d40c98a4ad5bed849105ce0d910da914
Chase Southard <chase.southard@gmail.com>
2025-05-08 15:34:58 -0400

    Updates class documentation

--- lib/omniauth/strategies/github_data_residency.rb
+++ lib/omniauth/strategies/github_data_residency.rb
@@ -3,7 +3,6 @@ require "omniauth/strategies/github"
 module OmniAuth
   module Strategies
     class GitHubDataResidency < OmniAuth::Strategies::GitHub
-      # The enterprise options are set in the setup phase proc (see: config/initializers/devise.rb) from the request params either enterprise_subdomain or github_host
       # The enterprise subdomain is used to build the client_options urls for the GitHub Enterprise with Data Residency OAuth API
       # The enterprise_subdomain is required to be set in the request params for the GitHubDR strategy to work
       option :enterprise_options, {
@@ -15,8 +14,8 @@ module OmniAuth
         token_url: "https://github.com/login/oauth/access_token"
       }
 
-      # Override the setup_phase to build the client options before calling super.
-      # This is necessary to ensure that the correct client options are used for the request.
+      # Override the request_phase to build the client_options before calling super.
+      # This is necessary to ensure that the correct client_options are used for the request.
       # Specifically, we need to set the redirect_uri with the callback URL to include the enterprise subdomain and ghe.com domain.
       # https://github.com/omniauth/omniauth-oauth2/blob/master/lib/omniauth/strategies/oauth2.rb#L58-L60
       def request_phase
@@ -27,8 +26,8 @@ module OmniAuth
         super
       end
 
-      # Override the callback_phase to build the client options before calling super.
-      # This is necessary to ensure that the correct client options are used for the callback request.
+      # Override the callback_phase to build the client_options before calling super.
+      # This is necessary to ensure that the correct client_options are used for the callback request.
       # Specifically, we need to set the redirect_uri with the callback URL to include the enterprise subdomain and ghe.com domain when
       #   we build the access token.
       # https://github.com/omniauth/omniauth-oauth2/blob/master/lib/omniauth/strategies/oauth2.rb#L124-L127
@@ -44,8 +43,9 @@ module OmniAuth
       protected
 
       # Set client_options with the enterprise_subdomain and the ghe.com domain
+      # Raising an OAuth2::CallbackError will stop the callback_phase and it will prevent the redirect_uri from being set to an invalid url
       def build_client_options
-        raise OAuth2::CallbackError.new("An Enterprise subdomain is required.") if invalid_enterprise_subdomain?
+        raise OmniAuth::Strategies::OAuth2::CallbackError.new("An Enterprise subdomain is required.") if invalid_enterprise_subdomain?
 
         options.client_options.site = "https://api.#{options.enterprise_options.enterprise_subdomain}.ghe.com"
         options.client_options.authorize_url = "https://#{options.enterprise_options.enterprise_subdomain}.ghe.com/login/oauth/authorize"
